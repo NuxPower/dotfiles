c5da9b49a6d37cf4b19628eeceb82249e3131b78
hanyu <nahoynikulcallanta@gmail.com>
2025-12-29 19:42:12 +0800

    update: 2025-12-29 19:42

--- push.sh
+++ push.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# 1. Update package lists
+# Update the package lists
 pacman -Qqen > ~/dotfiles/pkg-lists/pacman_list.txt
 pacman -Qqem > ~/dotfiles/pkg-lists/aur_list.txt
 
-# 2. Push to GitHub
-cd ~/dotfiles
+# Sync to GitHub
 git add .
-git commit -m "Auto-update: $(date +'%Y-%m-%d %H:%M')"
-git push origin main
+git commit -m "update: $(date +'%Y-%m-%d %H:%M')"
+git push
