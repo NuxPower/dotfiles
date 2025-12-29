0592f7a0a34b5f28913b0b958f2c5f72cc1a976e
hanyu <nahoynikulcallanta@gmail.com>
2025-12-29 19:48:48 +0800

    Auto-update: 2025-12-29 19:48

--- push.sh
+++ push.sh
@@ -4,6 +4,11 @@ pacman -Qqen > ~/dotfiles/pkg-lists/pacman_list.txt
 pacman -Qqem > ~/dotfiles/pkg-lists/aur_list.txt
 
 # Sync to GitHub
+cd ~/dotfiles
 git add .
-git commit -m "update: $(date +'%Y-%m-%d %H:%M')"
-git push
+if git commit -m "Auto-update: $(date +'%Y-%m-%d %H:%M')"; then
+    git push origin main
+    notify-send "Dotfiles Backup" "Successfully pushed to GitHub!" --icon=org.kde.plasma.emojier
+else
+    notify-send "Dotfiles Backup" "No changes to push." --icon=dialog-information
+fi
